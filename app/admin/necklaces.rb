ba6c76ec3fed0313c48bdc09b568d780fdf59b3b
anonymous anonymous <maya.patel@kellogg.northwestern.edu>
2022-03-14 17:21:42 +0000

    Whitelist columns in Necklace for ActiveAdmin

--- app/admin/necklaces.rb
+++ app/admin/necklaces.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Necklace do
 
+ permit_params :category_id, :title, :price, :image
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
