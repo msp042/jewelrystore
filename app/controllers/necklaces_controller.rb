ac742d945b28dff8867534cbe3337609bb588bb2
anonymous anonymous <maya.patel@kellogg.northwestern.edu>
2022-03-14 17:21:53 +0000

    Display associated collections on show pages

--- app/controllers/necklaces_controller.rb
+++ app/controllers/necklaces_controller.rb
@@ -42,8 +42,14 @@ class NecklacesController < ApplicationController
   # DELETE /necklaces/1
   def destroy
     @necklace.destroy
-    redirect_to necklaces_url, notice: 'Necklace was successfully destroyed.'
+    message = "Necklace was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to necklaces_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
